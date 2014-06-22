e62ad20cf30cc818947d6836ef4547480ec17698
Shogo Yasuda <gt68bz@gmail.com>
2014-06-22 22:48:02 +0900

    change files for smartphone

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    puts request.env["HTTP_USER_AGENT"]
+
+    ua = request.env["HTTP_USER_AGENT"]
+    if(ua.include?('Mobile') || ua.include?('Android'))
+      render template: "welcome/index_s"
+    else
+      render template: "welcome/index"
+    end
+
   end
+
 end
