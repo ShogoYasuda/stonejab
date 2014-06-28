2182b01be78f87a33c1f0038dda91666dc5c7e55
Shogo Yasuda <gt68bz@gmail.com>
2014-06-28 13:18:23 +0900

    fix unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 worker_processes 2
 timeout 20
 preload_app false
-stdout_path "log/unicorn-out.log"
-stderr_path "log/unicorn-err.log"
+stdout_path "log/unicorn.stdout.log"
+stderr_path "log/unicorn.stderr.log"
