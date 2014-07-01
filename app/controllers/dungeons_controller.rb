29bc409d36902dcf36a367e4b709a938e7b6c565
Shogo Yasuda <gt68bz@gmail.com>
2014-07-01 18:27:52 +0900

    refactoring view files

--- app/controllers/dungeons_controller.rb
+++ app/controllers/dungeons_controller.rb
@@ -1,5 +1,13 @@
 class DungeonsController < ApplicationController
   def index
+
+    ua = request.env["HTTP_USER_AGENT"]
+    if(ua.include?('Mobile') || ua.include?('Android'))
+      render template: "dungeons/index_s"
+    else
+      render template: "dungeons/index"
+    end
+
   end
 
   def new
